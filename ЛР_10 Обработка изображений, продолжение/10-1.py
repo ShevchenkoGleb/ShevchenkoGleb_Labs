4293abcbd40be65ef175ffaccfed3189a3ea9ffd
Gleb <shevchenkogleb032@gmail.com>
2025-05-06 17:31:25 +0300

    Lab_10

--- ЛР_10 Обработка изображений, продолжение/10-1.py
+++ ЛР_10 Обработка изображений, продолжение/10-1.py
@@ -1,10 +1,10 @@
 from PIL import Image
 
-input_folder = "D:/АиП/Лабы/ЛР_9 Обработка изображений, продолжение/10-1/открытка.png"
+input_folder = "D:/Projects_1_MD_8/ЛР_10 Обработка изображений, продолжение/10-1/открытка.png"
 image=Image.open(input_folder)
 #image.show()
 print(f"Размер: {image.width} * {image.height}")
 cut=(270, 50, 670, 200)
 cut_image=image.crop(cut)
-cut_image.save('D:/АиП/Лабы/ЛР_9 Обработка изображений, продолжение/10-1/cut.png')
+cut_image.save('D:/Projects_1_MD_8/ЛР_10 Обработка изображений, продолжение/10-1/cut.png')
 cut_image.show()
